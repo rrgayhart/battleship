c5dc1a9b95961c4b180b2d6cc1939e2076aea64b
Meeka <rrgayhart@gmail.com>
2013-11-16 16:40:31 -0700

    Sets up first command prompt for setting up ships on the board

--- battleship.rb
+++ battleship.rb
@@ -3,13 +3,37 @@ require_relative 'ship'
 require_relative 'player'
 require_relative 'moves'
 
-include Moves
+class Battleship
+  attr_accessor :game
+  include Moves
 
-game = Game.new(12)
-game.display_board
+  def initialize
+    @game ||= Game.new(12)
+  end
 
-game.place_ship(5, 0, 0, 'h')
-game.place_ship(2, 4, 4, 'v')
+  def welcome
+    puts "Welcome to Battleship! There are two players 
+    and you sink some ships. What could be better than that?"
+    puts "Each player has 5 boats of different sizes and you try
+    to sink the other player's boats."
+    puts "Let's start with Player 1. Player 2... look away."
+    puts "Press any key to continue when you're ready..."
+    next_move
+    puts "Great. Let's place your five ships"
+  end
+
+  def next_move
+    input = gets.chomp
+  end
+
+end
+
+engine = Battleship.new
+game = engine.game
+engine.welcome
+
+# game.place_ship(5, 0, 0, 'h')
+# game.place_ship(2, 4, 4, 'v')
 
 
 
