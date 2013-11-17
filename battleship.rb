cc4f6784829de3b2e15dbdfcd1aec8ad1ed01459
Meeka <rrgayhart@gmail.com>
2013-11-17 00:04:08 -0700

    Ship placement part of gameplay created (for first player only)

--- battleship.rb
+++ battleship.rb
@@ -20,6 +20,24 @@ class Battleship
     puts "Press any key to continue when you're ready..."
     next_move
     puts "Great. Let's place your five ships"
+    ship_count = 5
+    5.times do
+      board_set_up(5)
+      ship_count -=1
+    end
+  end
+
+  def board_set_up(ship)
+    puts "Here is your board."
+    @game.display_board
+    puts "place your ship by picking 0-11"
+    x = next_move.to_i
+    puts "place your ship by picking 0-11"
+    y = next_move.to_i
+    puts "input v for vertical, h for horizontal"
+    orientation = next_move
+    @game.place_ship(5, x, y, orientation)
+    @game.display_board
   end
 
   def next_move
